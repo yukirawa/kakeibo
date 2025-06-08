4f7a2a5186ab1c511dd0047fef3f47a6fbb36e68
ゆきらわ <kuniwa56@gmail.com>
2025-06-08 09:10:16 +0900

    m

---
+++ config.js
@@ -0,0 +1,16 @@
+// Firebase設定
+const __firebase_config = {
+    apiKey: "AIzaSyC8xib8Xsp2SHVl4ICXVSYkoKHFCQavbVY",
+    authDomain: "kakeibo-app-20c96.firebaseapp.com",
+    projectId: "kakeibo-app-20c96",
+    storageBucket: "kakeibo-app-20c96.appspot.com",
+    messagingSenderId: "298884559373",
+    appId: "1:298884559373:web:df7ad151ac63e3fcb9db44",
+    measurementId: "G-G27RXBKGES"
+};
+
+// アプリケーションID
+const __app_id = "kakeibo-app-20c96";
+
+// 初期認証トークン（必要な場合）
+const __initial_auth_token = null; 
